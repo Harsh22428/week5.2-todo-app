9f59d35d61a00f1937e755528a472d229476e255
Harsh22428 <harsh.22428@knit.ac.in>
2024-08-01 15:12:11 +0530

    create validation of todo

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,18 @@
-const express=require("express")
+const express=require("express");
+const { createTodo, updateTodo } = require("./types");
 const app=express()
 app.use(express.json())
 
 app.post("/todo",(req,res)=>{
+    const createPayload=req.body;
+    const parsePayload=createTodo.safeParse(createTodo);
+    if(!parsePayload.success){
+        res.status(411).json({
+            msg:"you sent the wrong inputs",
+        })
+        return;
+
+    }
 
 })
 
@@ -10,9 +20,17 @@ app.get("/todos",(req,res)=>{
     res.send("hi")
     console.log("jh")
 })
-
 app.put("completed",(req,res)=>{
-    
+    const updatPayload=req.body;
+    const parsePayload=updateTodo.safeParse(updatPayload);
+    if(!parsePayload.success){
+        res.status(411).json({
+            msg:"you sent the wrong inputs"
+        })
+        return;
+    }
+
+
 })
 
 app.listen(3000);
---
+++ readme.md
@@ -0,0 +1,7 @@
+## ToDo app
+
+This app contain simple-to-do app, which contain following features:
+
+- Anyone can create a todo
+- Anyone can see their todos 
+- Anyone can mark as a done 
